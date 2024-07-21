27012fabde2b27ec5f12098e5015390d00a08530
Jake <jacobmcpeak98@gmail.com>
2024-07-20 21:30:05 -0400

    Add back button and month select

--- src/components/BarGraph/index.js
+++ src/components/BarGraph/index.js
@@ -1,18 +1,11 @@
 import React from 'react'
 
-import { CategoryScale, LinearScale, BarElement, Chart } from 'chart.js'
-
 import { BarChart, Bar, Rectangle, XAxis, YAxis, 
   CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 import usageData from '../../data/data.json'
 
 import './style.css'
-
-Chart.register(CategoryScale)
-Chart.register(LinearScale)
-
-Chart.register(BarElement)
 
 const processBarData = (options, rawData) => {
   const pokemon = rawData.byMonth[options.format][options.month][options.elo].slice(0, options.show) 
@@ -35,32 +28,32 @@ const BarGraph = ({ options, setOptions }) => {
 
   return (
     <div style={{width: "90%", height: data.length * 80}}>
-    <ResponsiveContainer width="100%" height="100%">
-      <BarChart
-        width={500}
-        height={300}
-        data={data}
-        layout="vertical"
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis type="number" />
-        <YAxis type="category" dataKey="name" />
-        <Tooltip />
-        <Legend />
-        <Bar 
-          onClick={onClick} 
-          dataKey="percent" 
-          fill="#8884d8" 
-          activeBar={<Rectangle fill="pink" stroke="blue" />} 
-          />
-      </BarChart>
-    </ResponsiveContainer>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          width={500}
+          height={300}
+          data={data}
+          layout="vertical"
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis type="number" />
+          <YAxis type="category" dataKey="name" />
+          <Tooltip />
+          <Legend />
+          <Bar 
+            onClick={onClick} 
+            dataKey="percent" 
+            fill="#8884d8" 
+            activeBar={<Rectangle fill="pink" stroke="blue" />} 
+            />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
   );
 }
--- src/components/GraphOptions/index.js
+++ src/components/GraphOptions/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import Dropdown from '../Dropdown'
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 import formats from '../../data/formats.json'
+import months from '../../data/months.json'
 
 const GraphOptions = ({ options, setOptions }) => {
 
@@ -15,26 +17,43 @@ const GraphOptions = ({ options, setOptions }) => {
     })
   }
 
+  const onClickBack = (ev) => {
+    setOptions({ ...options, graphMode: 'bar' })
+  }
+
+  const curFormat = formats.formats.filter(format => format.name === options.format)[0]
+
   const formatOptions = formats.formats.map(format => ({
     label: format.label,
     value: format.name
   }))
 
-  const eloOptions = formats.formats
-    .filter(format => format.name === options.format)[0]
-    .elos.map(elo => ({
+  const eloOptions = curFormat.elos.map(elo => ({
       label: "Above " + elo + " elo",
       value: elo
     }))
 
+  const monthOptions = months
+    .slice(months.findIndex(month => month === curFormat.firstMonth))
+    .map(month => ({
+      label: month,
+      value: month
+  }))
+
   return (
     <Row>
+      <Col>
+        {options.graphMode === 'bar' ?
+          <Dropdown title={'Month'} onSelect={buildOnSelect('month')} options={monthOptions} /> :
+          <Button onClick={onClickBack}>Back</Button>
+        }
+      </Col>
       <Col>
         <Dropdown title={'ELO'} onSelect={buildOnSelect('elo')} options={eloOptions} />
       </Col>
       <Col>
         <Dropdown title={'Game Format'} onSelect={buildOnSelect('format')} options={formatOptions} />
-      </Col>
+     </Col>
     </Row>
   )
 }
